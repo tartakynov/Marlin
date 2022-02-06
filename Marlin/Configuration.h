d5d834dd42f1ebb68a78c3b1de1875c2bd1e56b4
Artem Tartakynov <artem.tad@gmail.com>
2022-02-06 14:20:10 -0500

    don't specify Z nozzle to probe offset as it will be calibrated separately

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1181,7 +1181,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44.110, -6.170, -1.280 }
+#define NOZZLE_TO_PROBE_OFFSET { -44.110, -6.170, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
